95f03e5cd203961d687f68fd35d82ab1db9c8931
Nasser BinObied <19620026+nbinobied@users.noreply.github.com>
2020-11-25 12:34:28 +0300

    Init

---
+++ POCApi/Controllers/HomeController.cs
@@ -0,0 +1,97 @@
+﻿using DinkToPdf;
+using DinkToPdf.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using POCWeb.Models;
+using RazorLight;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace POCApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly IConverter _pdfConverter;
+
+        public HomeController(IConverter pdfConverter)
+        {
+            _pdfConverter = pdfConverter;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)],
+                BaseUrl = "http://localhost:55213"
+            })
+            .ToArray();
+        }
+
+        [Route("PrintPDF")]
+        public async Task<byte[]> PrintPDFAsync()
+        {
+            var engine = new RazorLightEngineBuilder()
+                            .UseFileSystemProject(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location))
+                            .UseMemoryCachingProvider()
+                            .Build();
+
+            var model = new List<WeatherForecast>(Get());
+            string page = null;
+            try
+            {
+                var path = Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), $"Views/Home/Index.cshtml");
+                page = await engine.CompileRenderAsync(path, model);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            var doc = new HtmlToPdfDocument()
+            {
+                GlobalSettings = {
+                    ColorMode = ColorMode.Color,
+                    Orientation = Orientation.Landscape,
+                    PaperSize = PaperKind.A4Plus
+                },
+                Objects = {
+                    new ObjectSettings() {
+                        PagesCount = true,
+                        HtmlContent = page,
+                        WebSettings = { DefaultEncoding = "utf-8" },
+                        HeaderSettings = { FontSize = 9, Right = "Page [page] of [toPage]", Line = true, Spacing = 2.812 }
+                    }
+                }
+            };
+
+            byte[] pdf = null;
+            try
+            {
+                pdf = _pdfConverter.Convert(doc);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            return pdf;
+        }
+    }
+}
---
+++ POCWeb/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using POCWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace POCWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: WeatherForecasts
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<WeatherForecast> weatherForecasts = null;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:55237/");
+                //HTTP GET
+                var responseTask = await client.GetAsync("Home");
+                if (responseTask.IsSuccessStatusCode)
+                {
+
+                    var read = await responseTask.Content.ReadAsStringAsync();
+                    weatherForecasts = JsonConvert.DeserializeObject<IEnumerable<WeatherForecast>>(read);
+                    return View(weatherForecasts);
+                }
+            }
+            return View(weatherForecasts);
+        }
+
+        public async Task<IActionResult> PrintPDFAsync()
+        {
+            byte[] file = null;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:55237/Home/");
+                HttpResponseMessage responseTask = await client.GetAsync("PrintPDF");
+                if (responseTask.IsSuccessStatusCode)
+                {
+                    string response = responseTask.Content.ReadAsStringAsync().Result.Replace("\"", string.Empty);
+                    file = Convert.FromBase64String(response);
+                }
+            }
+
+            //wrtite to desk for testing
+            //using (FileStream stream = new FileStream(@"C:\" + DateTime.UtcNow.Ticks.ToString() + ".pdf", FileMode.Create))
+            //{
+            //    stream.Write(file, 0, file.Length);
+            //}
+
+            return File(file, "application/pdf", "sample.pdf");
+        }
+    }
+}
